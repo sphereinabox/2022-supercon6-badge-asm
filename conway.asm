26c98b6c1bff15f0728771fa2e0ef7f767409dc5
Nick Winters <nwinters3000@gmail.com>
2022-11-05 12:28:34 -0700

    fill two buffers and swap between them
    I think using in/out was giving me issues.

--- conway.asm
+++ conway.asm
@@ -3,23 +3,23 @@
 ;;; 0xA0-0xBF fb1
 ;;; 0xC0-0xDF fb2
 ;;; registers:
-;;; R9 new page
-init:
-  ;; I'm not going to use OUT/IN for I/O
-  mov R0,0b0010                 ;
+;;; R9 next page to display
+;;; R8 previous page
+;;; display pages toggle between 0x2 and 0x4
+R8it:
+  ;; 0b1010 to turn off ALU display
+  ;; 0b0010 to just relocate R9/R8 to last page
+  mov R0,0b0010
   mov [0xF3],R0                 ; WRFLAGS
-  ;; todo: set up sync
+  ;; set up sync
   mov R0, 15
-  mov [0xF2], R0                ; sample code on 0xFC page puts this in F4?
+  mov [0xF2], R0                ; sample code on 0xFC page puts this into 0xF4?
   mov R0,0b0001                 ; low bit starts sync
-  mov [0xFC], R0                ; KeyStatus bit zero is handshaking bit for sync
+  mov [0xFC], R0                ; KeyStatus bit zero is handshakR8g bit for sync
 
   ;; page
-  mov R8,0xA                   ;next page to draw
-  ;; loop
-loop:
-  ;; todo: set old page register and new page register
-  mov R9,0xA
+  mov R9,0x2                   ;next page to draw
+  mov R8,0x4
 
   ;; fill page at R9 with random
   mov R1,R9
@@ -40,7 +40,36 @@ fill1:
   skip c,2
   goto fill1
 
-  ;; todo: swap active page
+  ;; fill page at R8 with random
+  mov R1,R8
+  mov R2,0
+fill2:
+  mov r0, [0xFF]
+  mov [R1:R2], R0
+  inc r2
+  skip c,2
+  goto fill2
+  ;; fill page at R8+1 with random
+  inc R1
+  mov R2,0
+fill3:
+  mov r0, [0xFF]
+  mov [R1:R2], R0
+  inc r2
+  skip c,2
+  goto fill3
+
+  ;; set display page to R9
+  mov R0,R9
+  mov [0xF0],R0
+
+loop:
+  ;; swap R8/R9 registers
+  mov r0,R9
+  mov R9,R8
+  mov R8,r0
+
+  ;; update page display
   mov R0,R9
   mov [0xF0],R0
 
